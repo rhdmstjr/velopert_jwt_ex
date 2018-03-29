50fe04c385ff3946d58a232aa4bdfad8829080fb
eunsuk Ko <dmstjr214@gmail.com>
2018-03-29 14:44:19 +0000

    Add Middleware for authentication
    middleware
    -hide authentication role from other functions

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const controller = require('./controller')
+const authMiddleware = require('../../../middlewares/auth')
 
 router.post('/register', controller.register)
 router.get('/register', controller.test)
@@ -7,6 +8,7 @@ router.get('/register', controller.test)
 //router.post('/login', controller.login)
 router.get('/login', controller.login_test)
 
+router.use('/check', authMiddleware)
 router.get('/check', controller.check)
 
 module.exports = router
